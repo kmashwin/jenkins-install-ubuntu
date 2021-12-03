1c8936d26580628f212cc58f8363381c731adeb6
ashwinkm <ashwinkabbinahalli@gmail.com>
2021-12-03 14:48:04 +0530

    adding jenkins-install-ubuntu.sh which is an initial script

---
+++ jenkins-install-ubuntu.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo apt update
+sudo apt install default-jre
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install jenkins
