5854ceb71018df44e91337498d0c534550150028
ashwinkm <ashwinkabbinahalli@gmail.com>
2021-12-03 16:57:01 +0530

    adding command to upgrade the packages and adding new repository for jenkins

--- jenkins-install-ubuntu.sh
+++ jenkins-install-ubuntu.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 sudo apt update
+sudo apt upgrade
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee \
+    /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+cho deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+    https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+    /etc/apt/sources.list.d/jenkins.list > /dev/null
 sudo apt install default-jre
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install jenkins
